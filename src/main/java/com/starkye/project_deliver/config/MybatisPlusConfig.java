a8ea9d0b80f2f95fb9158f44a5a2022253da09d5
HanchenYe <yehanchen@starkye.com>
2023-01-04 04:03:45 -0800

    DAY2 split page, search, use/ban employee, edit employee info

---
+++ src/main/java/com/starkye/project_deliver/config/MybatisPlusConfig.java
@@ -0,0 +1,16 @@
+package com.starkye.project_deliver.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+
+/**
+ * 配置mybatis 分页插件
+ */
+public class MybatisPlusConfig {
+
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return mybatisPlusInterceptor;
+    }
+}
--- src/main/java/com/starkye/project_deliver/controller/EmployeeController.java
+++ src/main/java/com/starkye/project_deliver/controller/EmployeeController.java
@@ -1,16 +1,16 @@
 package com.starkye.project_deliver.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.starkye.project_deliver.common.R;
 import com.starkye.project_deliver.entity.Employee;
 import com.starkye.project_deliver.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
 
@@ -105,5 +105,58 @@ public class EmployeeController {
         employeeService.save(employee);
 
         return R.success("添加员工成功");
+    }
+
+    /**
+     * 员工信息分页查询
+     * @param page
+     * @param pageSize
+     * @param name
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize, String name){
+        log.info("page = {},pageSize = {},name = {}",page,pageSize,name);
+
+        //构造分页构造器
+        Page pageinfo = new Page(page,pageSize);
+
+        //构造条件构造器
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper();
+        //添加一个过滤条件
+        queryWrapper.like(StringUtils.isNotEmpty(name), Employee::getName,name);
+        //排序条件
+        queryWrapper.orderByDesc(Employee::getUpdateTime);
+
+        //执行查询
+        employeeService.page(pageinfo,queryWrapper);
+
+        return R.success(pageinfo);
+    }
+
+    //根据ID修改员工信息
+    @PutMapping
+    public R<String> update(HttpServletRequest request, @RequestBody Employee employee){
+        log.info(employee.toString());
+
+        Long empID = (Long)request.getSession().getAttribute("employee");
+        employee.setUpdateTime(LocalDateTime.now());
+        employee.setUpdateUser(empID);
+        employeeService.updateById(employee);
+
+        return R.success("员工信息更改成功");
+    }
+
+    /**
+     * 根据ID查询员工信息
+      */
+    @GetMapping("/{id}")
+    public R<Employee> getByID(@PathVariable Long id){
+        log.info("根据ID查询员工信息...");
+        Employee employee = employeeService.getById(id);
+        if(employee != null){
+            return R.success(employee);
+        }
+        return R.error("No employee information");
     }
 }
